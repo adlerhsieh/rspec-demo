5e0eb7ce5c251d93bf9e6323fa3319050c5bbf7f
Adler <nkj20932@hotmail.com>
2015-05-28 00:20:42 +0800

    add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413125322) do
+ActiveRecord::Schema.define(version: 20150527161258) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
