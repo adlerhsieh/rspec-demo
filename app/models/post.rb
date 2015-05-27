58eea2c7d4ca3459ea777b93b24c209466d08124
Adler <nkj20932@hotmail.com>
2015-05-28 00:20:54 +0800

    add post model test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  validates :title, :presence => true
+  has_many :comments
+
+  def self.no_content
+    where(:content => nil)
+  end
+
+  def abstract
+    self.content[0..20]
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 	it "is accessible" do
-		post = Post.create!
+		post = Post.create!(:title => "title")
 		expect(post).to eq(Post.last)
 	end
 
@@ -12,5 +12,29 @@ RSpec.describe Post, type: :model do
 		expect(columns).to include("title")
 		expect(columns).to include("content")
 		expect(columns).not_to include("user_id")
+	end
+
+	it "validates title" do
+    expect(Post.new).not_to be_valid
+    expect(Post.new(:title => "title")).to be_valid
+	end
+
+	it ".no_content" do
+    post_with_content = Post.create(:title => "title", :content => "content")
+		post_without_content = Post.create(:title => "title", :content => nil)    
+	  expect(Post.no_content).to include post_without_content
+	  expect(Post.no_content).not_to include post_with_content
+	end
+
+	it "#abstract" do
+		post = Post.create(:title => "title", :content => "12345678901234567890_not_abstract")
+		expect(post.abstract).to include("12345678901234567890")
+		expect(post.abstract).not_to include("not_abstract")
+	end
+
+	it "has_many comments" do
+    post = Post.create(:title => "title", :content => "content")
+    comment = Comment.create(:content => "content", :post_id => post.id)
+    expect(post.comments).to include(comment)
 	end
 end
